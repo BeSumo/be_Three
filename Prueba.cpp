de8403aab9e23ca8e8b6f4b8a359f26d186ea7e9
BeSumo <166965052+BeSumo@users.noreply.github.com>
2024-05-13 00:59:01 -0400

    Create Prueba.cpp
    Prueba

---
+++ Prueba.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct Raiz{
+	int dato;
+	Raiz* izquierdo;
+	Raiz* derecho;	
+}
+
+Raiz *nuevoHijo(Raiz*inicio,int dato){
+	Raiz *NuevoHijo = new Raiz;
+	NuevoHijo->dato;
+	NuevoHijo->izquierdo;
+	NuevoHijo->derecho;
+}
+
+
+int main(){
+	return 0;
+}
